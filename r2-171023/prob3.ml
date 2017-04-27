b0b39b5aca5f2bac5226d7835306d9bb6e781e12
Fujii Yosuke <fujiyou1121@gmail.com>
2017-04-27 11:15:16 +0900

    rep 2 prob 3 done

--- r2-171023/prob3.ml
+++ r2-171023/prob3.ml
@@ -1,40 +1,22 @@
 type 'a tree =
   | Leaf
   | Node of 'a * 'a tree * 'a tree;;
-exception OrderError
 
-let rec map f arr =
+
+let rec rev arr acc =
   match arr with
-  | [] -> []
-  | x :: xs -> f x :: map f xs
+  | [] -> acc
+  | x :: xs -> rev xs (x :: acc);;
 
-let rec filter f arr =
-  match arr with
-  | [] -> []
-  | x :: xs when f x -> x :: filter f xs
-  | x :: xs -> filter f xs;;
+(* 再帰バージョン *)
+let reverse arr = rev arr [];;
 
-(* レベル順は幅優先と同じ順番 *)
-(* 調べた所関数型ではキューの代わりに余再帰というものを使えば実現可能ということでそれを書いてみる *)
+let rec levelacc que acc =
+  match que with
+  | [] -> acc
+  | tr :: xs ->
+    match tr with
+      | Leaf -> levelacc xs acc
+      | Node (x, l, r) -> levelacc (xs @ (l :: r :: [])) (x :: acc);;
 
-
-let rec walk n t =
-  if n = 0 then []
-  else match t with
-    | [] -> []
-    | Leaf :: xs -> walk (n - 1) xs
-    | (Node (_, l, r)) :: xs -> l :: r :: walk (n + 1) xs;;
-
-(* 一応エラー処理 *)
-let extract t =
-  match t with
-  | Node (x, _, _) -> x
-  | _ -> raise OrderError;;
-
-let isNode t =
-  match t with
-  | Leaf -> false
-  | Node (_, _, _) -> true;;
-
-let levelorder t = map extract (filter isNode (
-  ));;
+let levelorder tr = reverse (levelacc (tr :: []) []);;
